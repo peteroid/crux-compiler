354e6b0f60325cf2e9707b36b1ec48a57fea6cee
PETEroid <selllfocard@gmail.com>
2016-01-21 15:51:52 -0800

    Update w/o test

--- src/crux/Parser.java
+++ src/crux/Parser.java
@@ -147,11 +147,18 @@ public class Parser {
     }
    
 // Grammar Rules =====================================================
+
+    public void simpleGrammar(NonTerminal nt)
+    {
+        enterRule(nt);
+        expect(nt);
+        exitRule(nt);
+    }
     
     // literal := INTEGER | FLOAT | TRUE | FALSE .
     public void literal()
     {
-        throw new RuntimeException("implement this");
+        simpleGrammar(NonTerminal.LITERAL);
     }
     
     // designator := IDENTIFIER { "[" expression0 "]" } .
@@ -168,10 +175,355 @@ public class Parser {
         exitRule(NonTerminal.DESIGNATOR);
     }
 
+    // type := IDENTIFIER .
+    public void type()
+    {
+        simpleGrammar(NonTerminal.TYPE);
+    }
+
+    // op0 := ">=" | "<=" | "!=" | "==" | ">" | "<" .
+    public void op0()
+    {
+        simpleGrammar(NonTerminal.OP0);
+    }
+
+    // op1 := "+" | "-" | "or" .
+    public void op1()
+    {
+        simpleGrammar(NonTerminal.OP1);
+    }
+
+    // op2 := "*" | "/" | "and" .
+    public void op2()
+    {
+        simpleGrammar(NonTerminal.OP2);
+    }
+
+    // expression0 := expression1 [ op0 expression1 ] .
+    public void expression0()
+    {
+        enterRule(NonTerminal.EXPRESSION0);
+
+        expression1();
+        if(accept(NonTerminal.OP0))
+            expression1();
+
+
+        exitRule(NonTerminal.EXPRESSION0);
+    }
+
+    // expression1 := expression2 { op1  expression2 } .
+    public void expression1()
+    {
+        enterRule(NonTerminal.EXPRESSION1);
+
+        expression2();
+        while(accept(NonTerminal.OP1))
+            expression2();
+
+
+        exitRule(NonTerminal.EXPRESSION1);
+    }
+
+    // expression2 := expression3 { op2 expression3 } .
+    public void expression2()
+    {
+        enterRule(NonTerminal.EXPRESSION2);
+
+        expression3();
+        while(accept(NonTerminal.OP2))
+            expression3();
+
+
+        exitRule(NonTerminal.EXPRESSION2);
+    }
+
+    /* expression3 := "not" expression3
+     * | "(" expression0 ")"
+     * | designator
+     * | call-expression
+     * | literal .
+     */
+    public void expression3()
+    {
+        enterRule(NonTerminal.EXPRESSION3);
+
+        if (accept(Token.Kind.NOT)){
+            expression3();
+        } else if (accept(Token.Kind.OPEN_PAREN)) {
+            expression0();
+            expect(Token.Kind.CLOSE_PAREN);
+        } else if (accept(NonTerminal.DESIGNATOR)) {
+            designator();
+        } else if (accept(NonTerminal.CALL_EXPRESSION)) {
+            callExpression();
+        } else if (accept(NonTerminal.LITERAL)) {
+            literal();
+        } else {
+            throw new QuitParseException(reportSyntaxError(NonTerminal.EXPRESSION3));
+        }
+
+        exitRule(NonTerminal.EXPRESSION3);
+    }
+
+    // call-expression := "::" IDENTIFIER "(" expression-list ")" .
+    public void callExpression()
+    {
+        enterRule(NonTerminal.CALL_EXPRESSION);
+
+        expect(NonTerminal.CALL_EXPRESSION);
+        expect(Token.Kind.IDENTIFIER);
+        expect(Token.Kind.OPEN_PAREN);
+        expressionList();
+        expect(Token.Kind.CLOSE_PAREN);
+
+        exitRule(NonTerminal.CALL_EXPRESSION);
+    }
+
+    // expression-list := [ expression0 { "," expression0 } ] .
+    public void expressionList()
+    {
+        enterRule(NonTerminal.EXPRESSION_LIST);
+
+        if (accept(NonTerminal.EXPRESSION0)){
+            expression0();
+            while (accept(Token.Kind.COMMA))
+                expression0();
+        }
+
+        exitRule(NonTerminal.EXPRESSION_LIST);
+    }
+
+    // parameter := IDENTIFIER ":" type .
+    public void parameter()
+    {
+        enterRule(NonTerminal.PARAMETER);
+
+        expect(Token.Kind.IDENTIFIER);
+        expect(Token.Kind.COLON);
+        type();
+
+        exitRule(NonTerminal.PARAMETER);
+    }
+
+    // parameter-list := [ parameter { "," parameter } ] .
+    public void parameterList()
+    {
+        enterRule(NonTerminal.PARAMETER_LIST);
+
+        if (accept(NonTerminal.PARAMETER)){
+            parameter();
+            while (accept(Token.Kind.COMMA))
+                parameter();
+        }
+
+        exitRule(NonTerminal.PARAMETER_LIST);
+    }
+
+    // variable-declaration := "var" IDENTIFIER ":" type ";"
+    public void variableDeclaration()
+    {
+        enterRule(NonTerminal.VARIABLE_DECLARATION);
+
+        expect(Token.Kind.VAR);
+        expect(Token.Kind.IDENTIFIER);
+        expect(Token.Kind.COLON);
+        type();
+        expect(Token.Kind.SEMICOLON);
+
+        exitRule(NonTerminal.VARIABLE_DECLARATION);
+    }
+
+    // array-declaration := "array" IDENTIFIER ":" type "[" INTEGER "]" { "[" INTEGER "]" } ";"
+    public void arrayDeclaration()
+    {
+        enterRule(NonTerminal.ARRAY_DECLARATION);
+
+        expect(Token.Kind.ARRAY);
+        expect(Token.Kind.IDENTIFIER);
+        expect(Token.Kind.COLON);
+        type();
+        expect(Token.Kind.OPEN_BRACKET);
+        expect(Token.Kind.INTEGER);
+        expect(Token.Kind.CLOSE_BRACKET);
+        while (accept(Token.Kind.OPEN_BRACKET)) {
+            expect(Token.Kind.INTEGER);
+            expect(Token.Kind.CLOSE_BRACKET);
+        }
+        expect(Token.Kind.SEMICOLON);
+
+        exitRule(NonTerminal.ARRAY_DECLARATION);
+    }
+
+    // function-definition := "func" IDENTIFIER "(" parameter-list ")" ":" type statement-block .
+    public void functionDefinition()
+    {
+        enterRule(NonTerminal.FUNCTION_DEFINITION);
+
+        expect(Token.Kind.FUNC);
+        expect(Token.Kind.IDENTIFIER);
+        expect(Token.Kind.OPEN_PAREN);
+        parameterList();
+        expect(Token.Kind.CLOSE_PAREN);
+        expect(Token.Kind.COLON);
+        type();
+        statementBlock();
+
+        exitRule(NonTerminal.FUNCTION_DEFINITION);
+    }
+
+    // declaration := variable-declaration | array-declaration | function-definition .
+    public void declaration()
+    {
+        enterRule(NonTerminal.DECLARATION);
+
+        if (accept(NonTerminal.VARIABLE_DECLARATION))
+            variableDeclaration();
+        else if (accept(NonTerminal.ARRAY_DECLARATION))
+            arrayDeclaration();
+        else if (accept(NonTerminal.FUNCTION_DEFINITION))
+            functionDefinition();
+        else
+            throw new QuitParseException(reportSyntaxError(NonTerminal.DECLARATION));
+
+        exitRule(NonTerminal.DECLARATION);
+    }
+
+    // declaration-list := { declaration } .
+    public void declarationList()
+    {
+        enterRule(NonTerminal.DECLARATION_LIST);
+
+        while (accept(NonTerminal.DECLARATION))
+            declaration();
+
+        exitRule(NonTerminal.DECLARATION_LIST);
+    }
+
+    // assignment-statement := "let" designator "=" expression0 ";"
+    public void assignmentStatement()
+    {
+        enterRule(NonTerminal.ASSIGNMENT_STATEMENT);
+
+        expect(Token.Kind.LET);
+        designator();
+        expect(Token.Kind.EQUAL);
+        expression0();
+        expect(Token.Kind.SEMICOLON);
+
+        exitRule(NonTerminal.ASSIGNMENT_STATEMENT);
+    }
+
+    // call-statement := call-expression ";"
+    public void callStatement()
+    {
+        enterRule(NonTerminal.CALL_STATEMENT);
+
+        expect(NonTerminal.CALL_EXPRESSION);
+        expect(Token.Kind.SEMICOLON);
+
+        exitRule(NonTerminal.CALL_STATEMENT);
+    }
+
+    // if-statement := "if" expression0 statement-block [ "else" statement-block ] .
+    public void ifStatement()
+    {
+        enterRule(NonTerminal.IF_STATEMENT);
+
+        expect(Token.Kind.IF);
+        expression0();
+        statementBlock();
+        if (accept(Token.Kind.ELSE))
+            statementBlock();
+
+        exitRule(NonTerminal.IF_STATEMENT);
+    }
+
+    // while-statement := "while" expression0 statement-block .
+    public void whileStatement()
+    {
+        enterRule(NonTerminal.WHILE_STATEMENT);
+
+        expect(Token.Kind.WHILE);
+        expression0();
+        statementBlock();
+
+        exitRule(NonTerminal.WHILE_STATEMENT);
+    }
+
+    // return-statement := "return" expression0 ";" .
+    public void returnStatement()
+    {
+        enterRule(NonTerminal.RETURN_STATEMENT);
+
+        expect(Token.Kind.RETURN);
+        expression0();
+        expect(Token.Kind.SEMICOLON);
+
+        exitRule(NonTerminal.RETURN_STATEMENT);
+    }
+
+    /* statement := variable-declaration
+     * | call-statement
+     * | assignment-statement
+     * | if-statement
+     * | while-statement
+     * | return-statement .
+     */
+    public void statement()
+    {
+        enterRule(NonTerminal.VARIABLE_DECLARATION);
+
+        if (accept(NonTerminal.VARIABLE_DECLARATION))
+            variableDeclaration();
+        else if (accept(NonTerminal.CALL_STATEMENT))
+            callStatement();
+        else if (accept(NonTerminal.ASSIGNMENT_STATEMENT))
+            assignmentStatement();
+        else if (accept(NonTerminal.IF_STATEMENT))
+            ifStatement();
+        else if (accept(NonTerminal.WHILE_STATEMENT))
+            whileStatement();
+        else if (accept(NonTerminal.RETURN_STATEMENT))
+            returnStatement();
+        else
+            throw new QuitParseException(reportSyntaxError(NonTerminal.STATEMENT));
+
+        exitRule(NonTerminal.VARIABLE_DECLARATION);
+    }
+
+    // statement-list := { statement } .
+    public void statementList()
+    {
+        enterRule(NonTerminal.STATEMENT_LIST);
+
+        while (accept(NonTerminal.STATEMENT))
+            statement();
+
+        exitRule(NonTerminal.STATEMENT_LIST);
+    }
+
+    // statement-block := "{" statement-list "}" .
+    public void statementBlock()
+    {
+        enterRule(NonTerminal.STATEMENT_BLOCK);
+
+        expect(Token.Kind.OPEN_BRACE);
+        statementList();
+        expect(Token.Kind.CLOSE_BRACE);
+
+        exitRule(NonTerminal.STATEMENT_BLOCK);
+    }
+
     // program := declaration-list EOF .
     public void program()
     {
-        throw new RuntimeException("implement all the grammar rules");
+        enterRule(NonTerminal.PROGRAM);
+
+        declarationList();
+        expect(Token.Kind.EOF);
+
+        exitRule(NonTerminal.PROGRAM);
     }
     
 }
